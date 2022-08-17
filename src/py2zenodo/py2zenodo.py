86a61928d2a12bfa63c2693b57b6445fac8777ef
RemyLau <remylau961@gmail.com>
2022-08-17 08:02:49 -0400

    feat: barebone file load to zenodo

---
+++ src/py2zenodo/py2zenodo.py
@@ -0,0 +1,34 @@
+import os
+
+import requests
+from tqdm import tqdm
+from tqdm.utils import CallbackIOWrapper
+
+
+ZENODO_URL = "https://zenodo.org/api/deposit/depositions"
+SANDBOX_URL = "https://sandbox.zenodo.org/api/deposit/depositions"
+
+
+def upload(filepath, access_token):
+    params = {"access_token": access_token}
+    print(f"{params=!r}")
+
+    r = requests.post(SANDBOX_URL, params=params, json={})  #, headers={"Content-Type": "application/json"})
+    if not r.ok:
+        msg = r.json().get("message", "")
+        raise requests.exceptions.HTTPError(f"{r!r} {msg}")
+
+    # TODO: check if file exist
+    file_size = os.stat(filepath).st_size
+    print(f"{file_size=}")
+
+    # Upload file with progress bar, see
+    # https://gist.github.com/tyhoff/b757e6af83c1fd2b7b83057adf02c139
+    bucket_url = r.json()["links"]["bucket"]
+    upload_url = f"{bucket_url}/{filepath.name}"
+    with open(filepath, "rb") as fp:
+        with tqdm(total=file_size, unit="B", unit_scale=True, unit_divisor=1024) as t:
+            wrapped_file = CallbackIOWrapper(t.update, fp, "read")
+            requests.put(upload_url, data=wrapped_file, params=params)
+
+    print("Done.")
