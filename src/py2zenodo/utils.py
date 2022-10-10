1cf10c9c552aefb4f7e12984257543d8c8934129
RemyLau <remylau961@gmail.com>
2022-10-10 09:51:24 -0400

    implement config and utils

---
+++ src/py2zenodo/utils.py
@@ -0,0 +1,16 @@
+from urllib.parse import urljoin
+
+from py2zenodo import config
+
+logger = config.get_logger(__name__)
+
+
+def get_url(where, /, *, sandbox: bool = False) -> str:
+    if (url := config.URL_DICT.get(where)) is None:
+        raise ValueError(f"Invalid url option {where!r}")
+
+    url_base = config.URL_BASE_SANDBOX if sandbox else config.URL_BASE_ZENODO
+    url_full = urljoin(url_base, url)
+    logger.debug(f"{where=}, {sandbox=}, url: {url_full}")
+
+    return url_full
