a2276443cf6056b6813ccb8846c7f56646577eab
RemyLau <remylau961@gmail.com>
2022-10-10 18:53:04 -0400

    check files before creating depo

--- src/py2zenodo/cli.py
+++ src/py2zenodo/cli.py
@@ -3,6 +3,7 @@ from typing import Tuple
 import click
 
 from py2zenodo import Deposition
+from py2zenodo.utils import check_files
 
 
 @click.command()
@@ -10,9 +11,9 @@ from py2zenodo import Deposition
 @click.option("-t", "--token", help="Access token.")
 @click.option("-s", "--sandbox", is_flag=True, help="Use sandbox.")
 @click.option("-v", "--verbose", is_flag=True)
-def main(files: Tuple[str], token: str, sandbox: bool, verbose: bool):
+def main(files: Tuple[str, ...], token: str, sandbox: bool, verbose: bool):
+    check_files(files)
     depo = Deposition(access_token=token, sandbox=sandbox)
     depo.create_new_depo(verbose=verbose)
-    # TODO: make sure all files exist
     for file in files:
         depo.upload_file(file)
