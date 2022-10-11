e81316a417c999fa8d82df6a2156721f0d216c65
RemyLau <remylau961@gmail.com>
2022-10-10 21:07:33 -0400

    refactor BaseDepositonEntity

--- src/py2zenodo/entities/base.py
+++ src/py2zenodo/entities/base.py
@@ -1,2 +1,38 @@
+from typing import Any, Dict, Optional
+
+import requests
+
+from py2zenodo import utils
+
+
 class BaseEntity:
-    pass
+
+    url_key: str = "none"
+
+    def __init__(self, access_token: Optional[str], sandbox: bool = False):
+        self.access_token = access_token
+        self.api_url = utils.get_url(self.url_key, sandbox=sandbox)
+        self._r = None
+
+    @property
+    def connected(self) -> bool:
+        return self.r is not None and self.r.ok
+
+    @property
+    def r(self) -> Optional[requests.Response]:
+        return self._r
+
+    def get_params(self, **kwargs) -> Dict[str, Any]:
+        params = {"access_token": self.access_token}
+        params.update({i: j for i, j in kwargs.items() if j is not None})
+        return params
+
+
+class BaseDepositionEntity(BaseEntity):
+
+    url_key: str = "depositions"
+
+
+class BaseRecordEntity(BaseEntity):
+
+    url_key: str = "records"
